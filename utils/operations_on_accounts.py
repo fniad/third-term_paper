a29c33443ecc74fff0b676add36deb79623656a9
fniad <artkamproject@gmail.com>
2023-05-06 01:17:17 +0300

    move file to dir utils

--- utils/operations_on_accounts.py
+++ utils/operations_on_accounts.py
@@ -1,19 +1,13 @@
-import os
-from utils.utils_ import load_operations
-
-OPERATIONS_DATA_PATH = os.path.join('data/operations.json')
+from utils.load_json_util import load_operations
 
 
-def get_the_last_five_transactions(path=OPERATIONS_DATA_PATH):
-    operation_list = load_operations(OPERATIONS_DATA_PATH)
+def get_the_last_five_transactions(path):
+    operation_list = load_operations(path)
 
     five_transaction = ""
 
     for operation in operation_list[:5]:
         transaction = operation.get_result_operation()
-        five_transaction += f"{transaction} \n\n"
+        five_transaction += f"{transaction}\n\n"
 
     return five_transaction
-
-
-
