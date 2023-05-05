0e7ff52b919b31d8b2b8a8b30a284b2d1d6edf93
fniad <artkamproject@gmail.com>
2023-05-06 01:20:46 +0300

    update get_mask_card funcs

--- utils/utils_mask.py
+++ utils/utils_mask.py
@@ -15,6 +15,6 @@ def get_mask_card(from_or_to_card):
     first_six_digit = 6 * '*'
     str_digit = re.sub(r'\D', '', from_or_to_card)
     str_alpha = re.sub(r'\d+', '', from_or_to_card)
-    mask_digit = first_six_digit + str_digit[7:-3] + last_four_digit
+    mask_digit = first_six_digit + str_digit[6:-4] + last_four_digit
     masked_number = ' '.join([mask_digit[i:i + 4] for i in range(0, len(mask_digit), 4)])
     return f'{str_alpha}{masked_number}'
